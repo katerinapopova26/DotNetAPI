48cd496df5c4c29d568d59e0605e80f7739db912
Katerina Popova <katerina_26popova@yahoo.com>
2025-06-21 12:25:54 +0200

    API started

---
+++ Controllers/UserController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class UserController : ControllerBase
+{
+    DataContextDapper _dapper;
+    public UserController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+
+    [HttpGet("TestConnection")]
+
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>(" SELECT GETDATE()");
+    }
+
+    [HttpGet("GetUsers/{testValue}")]
+    // public ActionResult Test()
+     public string[] GetUsers(string testValue)
+
+    {
+        string[] responseArray = new string[] {
+"test1",
+"test2",
+testValue
+        };
+        return responseArray;
+    }
+
+}
+
+
